e70c167f047418e2f23e530859b4cd6e757a1520
Alexander-Nabatov <nabatovaleh23@gmail.com>
2022-04-05 21:26:53 +0300

    cms

--- viev.js
+++
@@ -1,6 +0,0 @@
-window.onload = function() {
-    var returnObj = JSON.parse(localStorage.getItem("myKey"));
-    if (returnObj == 1) {
-        document.location.href = "/viev-new.html";
-    }
- };
